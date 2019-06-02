e6321fefc0915c12c0778f7f5892c866db083adf
Lucas Rezende de Paula Silva <lucas.rpsilva@hotmail.com>
2019-06-02 09:23:57 -0300

    Add files via upload

--- testedev/js/main.js
+++ testedev/js/main.js
@@ -3,7 +3,9 @@ $(document).ready(function(){
     $("#convites").hide();
     $("#salvar2").hide();
     $("#deletar").hide();
-    var key = 1;
+    
+    var key = 0;
+    var dados = [];
     
     $("#membro").click(function(){
         $("#formulario").show();
@@ -40,6 +42,7 @@ $(document).ready(function(){
             var dep = $("#dep").val();
             var acesso = $("#acesso").val();
             $("#lista").append("<button class=" + "listaMembros " + "id=" + key + ">" + acesso + ": " + nome + " - " + email + " - " + dep + "</button>");
+            dados.push([key, acesso, nome, email, dep]);
             $("#lista").show();
             $("#salvar").hide();
             $("#salvar2").hide();
@@ -62,6 +65,10 @@ $(document).ready(function(){
         $("#membro").hide();
         $("#convites").hide();
         $("#formulario").show();
+        $("#acesso").val(dados[idElemento][1]);
+        $("#nome").val(dados[idElemento][2]);
+        $("#email").val(dados[idElemento][3]);
+        $("#dep").val(dados[idElemento][4]);
         $("#salvar").hide();
         $("#salvar2").show();
         $("#cancelar").show();
@@ -69,12 +76,16 @@ $(document).ready(function(){
         
         $("#deletar").click(function(){
             event.preventDefault();
-            $("#" + idElemento).remove();
+            $("#" + idElemento).detach();
             $("#salvar2").hide;
             $("#deletar").hide();
             $("#formulario").hide();
             $("#lista").show();
             $("#membro").show();
+            $("#nome").val("");
+            $("#email").val("");
+            $("#dep").val("");
+            $("#acesso").val("Usuário");
             if($(".listaMembros").length){
                 $("#convites").show();
                 $("#org").hide();
@@ -87,12 +98,13 @@ $(document).ready(function(){
         $("#salvar2").click(function(){
             $("#form").off().submit(function(){
                 event.preventDefault();
-                $("#" + idElemento).remove();
+                $("#" + idElemento).detach();
                 var nome = $("#nome").val();
                 var email = $("#email").val();
                 var dep = $("#dep").val();
                 var acesso = $("#acesso").val();
                 $("#lista").append("<button class=" + "listaMembros " + "id=" + idElemento + ">" + acesso + ": " + nome + " - " + email + " - " + dep + "</button>");
+                dados[idElemento] = [idElemento, acesso, nome, email, dep];
                 $("#lista").show();
                 $("#salvar").hide();
                 $("#salvar2").hide();
@@ -105,7 +117,6 @@ $(document).ready(function(){
                 $("#email").val("");
                 $("#dep").val("");
                 $("#acesso").val("Usuário");
-                key++;
             });
         });
     });
